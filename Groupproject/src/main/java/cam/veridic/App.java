3ce2c94fb5f163897b266e31090e5fb5cd578a57
VENKATA <saikadiyala1994@gmail.com>
2017-10-06 15:28:26 -0400

    removal of commit_whitespace

---
+++ Groupproject/src/main/java/cam/veridic/App.java
@@ -0,0 +1,14 @@
+package cam.veridic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+	system.out.println("Hai");
+    }
+}
